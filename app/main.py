df83eddbe647ecf77a8d94f95f94d0e6e14fa3b9
DI3273 <162439929+DI3273@users.noreply.github.com>
2024-04-12 16:47:27 +0900

    finally

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
+ 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return {"Message": "Hello root"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/home")
+def home():
+    return {"Message": "Home!"}
