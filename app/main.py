2dcea06c2585f48ad5546fea27a479a7ba01bb5c
DI3273 <162439929+DI3273@users.noreply.github.com>
2024-04-12 17:30:01 +0900

    what

--- app/main.py
+++ app/main.py
@@ -1,33 +1,15 @@
 from typing import Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-name ="nothing"
-number= 0
-
-
-@app.post("/")
-def create_item(item_id: str,q: int):
-    name = item_id
-    number = q   
-    return {"item_id": name, "q": number}
 
 @app.get("/")
-def read_item():
-    return {"item_id": name, "q": number}
+def read_root():
+    return {"Hello": "World"}
 
-@app.put("/")
-def update_item(item_id: str,q: int):
-    name = item_id
-    number = q
-    return {"item_id": name, "q": number}
 
-@app.delete_item("/")
-def read_item(item_id: str, q: int):
-    name = "nothing"
-    number = 0
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
