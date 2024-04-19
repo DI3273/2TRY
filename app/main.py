ba8054a6b6ca3b9327f0794c1f1b48cd47f7968c
DI3273 <162439929+DI3273@users.noreply.github.com>
2024-04-19 15:42:11 +0900

    1

--- app/main.py
+++ app/main.py
@@ -17,29 +17,11 @@ items = {}
 @app.get("/")
 def root():
     URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=9y8h8TX0vKGMY6hMRibiurfOGfySlt08cx43AN8x&returnType=json"
-@app.post("/items/{item_id}")
-def create_item(item_id: int, item: Item):
-    if item_id in items:
-        raise HTTPException(status_code=400, detail="Item already exists")
-    items[item_id] = item
-    return items[item_id]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    if item_id not in items:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return {"item_id": item_id, "q": q, "item": items[item_id]}
+    contents = requests.get(URL).text
+    
+    return { "message": contents}
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    if item_id not in items:
-        raise HTTPException(status_code=404, detail="Item not found")
-    items[item_id] = item
-    return {"item_id": item_id, "item": items[item_id]}
-
-@app.delete("/items/{item_id}")
-def delete_item(item_id: int):
-    if item_id not in items:
-        raise HTTPException(status_code=404, detail="Item not found")
-    del items[item_id]
-    return {"detail": "Item deleted"}
+@app.get("/home")
+def home():
+    return { "message": "Home!" }
